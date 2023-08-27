77d6e204edc9be41daf36f2625bd60e5a3a64100
Kei Abukawa <keichan37@gmail.com>
2023-08-27 13:06:30 +0900

    Initial Commit

---
+++ toEPUB/toEPUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  toEPUBApp.swift
+//  toEPUB
+//
+//  Created by 虻川景 on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct toEPUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
